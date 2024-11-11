05183849138ea624714e82b7858180a99d3c481f
MyronenkoHeorhii <s25954@pjwstk.edu.pl>
2024-11-11 01:16:37 +0100

    Initial commit

--- obj/Debug/net8.0/BYT_project.AssemblyInfo.cs
+++ obj/Debug/net8.0/BYT_project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BYT_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af47d4972ced79e19f4c057d1961df1d3cfc9fc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BYT_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BYT_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
